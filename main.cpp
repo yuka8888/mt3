7286c7df0867f6ce8199fd1a6b2417407c513044
yuka8888 <133742570+yuka8888@users.noreply.github.com>
2024-04-18 22:26:33 +0900

    MT3_00_03

--- main.cpp
+++ main.cpp
@@ -13,26 +13,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
-	Matrix4x4 m1, m2;
-
-	m1 = { 3.2f, 0.7f, 9.6f, 4.4f,
-		5.5f, 1.3f, 7.8f, 2.1f,
-		6.9f, 8.0f, 2.6f, 1.0f,
-		0.5f, 7.2f, 5.1f, 3.3f };
-
-	m2 = { 4.1f, 6.5f, 3.3f, 2.2f,
-	8.8f, 0.6f, 9.9f, 7.7f,
-	1.1f, 5.5f, 6.6f, 0.0f,
-	3.3f, 9.9f, 8.8f, 2.2f };
-
-	Matrix4x4 resultAdd = Add(m1, m2);
-	Matrix4x4 resultMultiply = Multiply(m1, m2);
-	Matrix4x4 resultSubtract = Subtract(m1, m2);
-	Matrix4x4 inverseM1 = Inverse(m1);
-	Matrix4x4 inverseM2 = Inverse(m2);
-	Matrix4x4 transposeM1 = Transpose(m1);
-	Matrix4x4 transposeM2 = Transpose(m2);
-	Matrix4x4 identity = MakeIdentity4x4();
+	Vector3 translate = { 4.1f, 2.6f, 0.8f };
+	Vector3 scale = { 1.5f, 5.2f, 7.3f };
+	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
+	Vector3 point = { 2.3f, 3.8f, 1.4f };
+	Matrix4x4 transformMatrix = {
+		1.0f, 2.0f, 3.0f, 4.0f,
+		3.0f, 1.0f, 1.0f, 2.0f,
+		1.0f, 4.0f, 2.0f, 3.0f,
+		2.0f, 2.0f, 1.0f, 3.0f,
+	};
+	Vector3 transform = Transform(point, transformMatrix);
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -54,15 +46,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		
-		MatrixScreenPrint(0, 0, resultAdd);
-		MatrixScreenPrint(0, kRowHeight * 5, resultSubtract);
-		MatrixScreenPrint(0, kRowHeight * 5 * 2, resultMultiply);
-		MatrixScreenPrint(0, kRowHeight * 5 * 3, inverseM1);
-		MatrixScreenPrint(0, kRowHeight * 5 * 4, inverseM2);
-		MatrixScreenPrint(kColumnWidth * 5, 0, transposeM1);
-		MatrixScreenPrint(kColumnWidth * 5, kRowHeight * 5, transposeM2);
-		MatrixScreenPrint(kColumnWidth * 5, kRowHeight * 5 * 2, identity);
+		VectorScreenPrintf(0, 0, transform, "transformed");
+		MatrixScreenPrint(0, kRowHeight * 5, translateMatrix);
+		MatrixScreenPrint(0, kRowHeight * 5 * 2, scaleMatrix);
 
 		///
 		/// ↑描画処理ここまで
--- mt.h
+++ mt.h
@@ -838,3 +838,62 @@ inline void MatrixScreenPrint(int x, int y, const Matrix4x4& matrix) {
 		}
 	}
 }
+
+/// <summary>
+/// 平行移動行列
+/// </summary>
+/// <param name="translate"></param>
+/// <returns></returns>
+inline Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
+	Matrix4x4 result = {};
+
+	result.m[0][0] = 1.0f;
+	result.m[1][1] = 1.0f;
+	result.m[2][2] = 1.0f;
+	result.m[3][0] = translate.x;
+	result.m[3][1] = translate.y;
+	result.m[3][2] = translate.z;
+	result.m[3][3] = 1.0f;
+
+	return result;
+}
+
+/// <summary>
+/// 拡大縮小行列
+/// </summary>
+/// <param name="scale"></param>
+/// <returns></returns>
+inline Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
+	Matrix4x4 result = {};
+
+	result.m[0][0] = scale.x;
+	result.m[1][1] = scale.y;
+	result.m[2][2] = scale.z;
+	result.m[3][3] = 1.0f;
+
+	return result;
+
+}
+
+/// <summary>
+/// 座標変換
+/// </summary>
+/// <param name="vector"></param>
+/// <param name="matrix"></param>
+/// <returns></returns>
+inline Vector3 Transform(const Vector3& vector, const Matrix4x4 matrix) {
+	Vector3 result = {};
+
+	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] + 1.0f * matrix.m[3][0];
+	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + 1.0f * matrix.m[3][1];
+	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + 1.0f * matrix.m[3][2];
+	float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] + 1.0f * matrix.m[3][3];
+	
+	assert(w != 0);
+
+	result.x /= w;
+	result.y /= w;
+	result.z /= w;
+
+	return result;
+}
